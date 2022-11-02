cf9b4c2521b06b39d29d25a660713c1793c1b039
EstelleMyddleware <estellegaits@myddleware.com>
2022-11-02 18:34:46 +0100

    validation test on User email address using a data provider

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace App\Tests\Entity;
 
 use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Test\CustomTestCase;
 use Symfony\Component\Validator\ConstraintViolation;
 
 /**
- * @covers User
+ * @covers App\Entity\User
  */
-final class UserTest extends KernelTestCase
+final class UserTest extends CustomTestCase
 {
     public function testItWorks(): void
     {
@@ -28,14 +28,35 @@ final class UserTest extends KernelTestCase
         $this->assertSame('michel', $user->getUsername());
         $this->assertSame('michel.vaillant@laposte.net', $user->getEmail());
         $this->assertContains('ROLE_FAST_LIFE', $user->getRoles(), 'A role is missing from the user\'s list of roles');
-        $this->validateUser($user);
+        $this->validateUser($user, true);
     }
-    
+
+    /**
+     * @dataProvider invalidEmailProvider
+     */
+    public function testEmailValidation(string $email): void
+    {
+        $user = $this->createUser('francis', 'testspwd', $email);
+        $this->validateUser($user, false);
+    }
+
+    public function invalidEmailProvider(): array
+    {
+        return [
+            ['test'],
+            ['test@email'],
+            ['@test.com'],
+            ['test email'],
+            ['1'],
+            ['1@1.']
+        ];
+    }
+
 
     /**
      * Ensures created User matches validation constraints
      */
-    public function validateUser(User $user): void
+    public function validateUser(User $user, bool $expectPassingValidation): void
     {
         self::bootKernel();
         $errors = $this->getContainer()->get('validator')->validate($user);
@@ -44,7 +65,12 @@ final class UserTest extends KernelTestCase
         foreach ($errors as $error) {
             $validationErrors[] = $error->getPropertyPath() . ' ' . $error->getMessage();
         }
-        $this->assertCount(0, $errors, implode(',', $validationErrors));
+        if ($expectPassingValidation){
+            $this->assertCount(0, $errors, implode(',', $validationErrors));
+        } else {
+            $this->assertNotNull($validationErrors);
+            $this->assertTrue((0 < count($validationErrors)));
+        }
         $this->assertInstanceOf(User::class, $user);
     }
 }
